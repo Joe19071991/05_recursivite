79065b25528906ebd4a3c33a6daa9dd809ed8fae
Joe19071991 <lauranove66@gmail.com>
2025-01-21 10:23:09 +0100

    premiere etape ajout de la fonction afficherDossier

---
+++ script.js
@@ -0,0 +1,56 @@
+const dossierPrincipal = {
+  nom: "Ada",
+  contenu: [
+    {
+      nom: "Projets collectifs Ada",
+      contenu: [
+        {
+          nom: "Pico8",
+          contenu: [
+            {
+              nom: "mariokart.p8",
+            },
+          ],
+        },
+        {
+          nom: "Dataviz",
+          contenu: [
+            {
+              nom: "index.html",
+            },
+            {
+              nom: "script.js",
+            },
+          ],
+        },
+      ],
+    },
+    { nom: "CV.pdf" },
+    {
+      nom: "Projets persos",
+      contenu: [
+        {
+          nom: "Portfolio",
+          contenu: [
+            {
+              nom: "index.html",
+            },
+            {
+              nom: "script.js",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+// console.log(dossierPrincipal.nom);
+// console.log(typeof dossierPrincipal);
+// console.log(dossierPrincipal);
+
+const afficherDossier = () => {
+  return dossierPrincipal.nom;
+};
+
+console.log(afficherDossier());
